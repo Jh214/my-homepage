1bd42a5e273d8909c4fa2e2d25bb61271547bfad
Jh214 <99015309+Jh214@users.noreply.github.com>
2022-03-05 14:37:48 +0900

    Show an error box if nothing is entered in the input tag

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 /* 자바스크립트는 웹과 사용자와 동적으로 상호작용 할 수 있게 해주는 언어이다. */
 console.log("java script");
+
+function Empty(){
+    if(frm.userid.value ==""){
+        frm.userid.focus();
+        document.getElementById("erMsg").style.display="block";
+        return false;
+    }
+}
